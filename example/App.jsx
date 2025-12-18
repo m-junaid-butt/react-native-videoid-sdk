2f26274f104f7fe951b9188ef529a23923b087c8
Fahad Azeem <fahadazeem.dev@gmail.com>
2025-12-18 17:31:11 +0500

    chore: example formatting

--- example/App.jsx
+++ example/App.jsx
@@ -45,7 +45,6 @@ const App = () => {
       const result = await VideoIDSDK.startWithCustomStyle(config, styleConfig);
 
       console.log('VideoID with custom style completed:', result);
-
       if (result.videoID) {
         Alert.alert('Success', `VideoID: ${result.videoID}`);
       } else {
